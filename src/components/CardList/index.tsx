2a1a511a264e06fee532d25d9df99fd72ccfa9d8
David Faria <davidfaria89@gmail.com>
2020-11-30 09:53:37 -0300

    feat: add component CardList

--- src/components/CardList/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles'
-
-const CardList = () => (
-  <S.Wrapper>
-    <h1>CardList</h1>
-  </S.Wrapper>
-)
-
-export default CardList
--- src/components/CardList/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import CardList from '.'
-
-export default {
-  title: 'CardList',
-  component: CardList
-} as Meta
-
-export const Default: Story = () => <CardList />
