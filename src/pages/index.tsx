6ca78ad442f0470aff322f3fc53fba0040e09919
David Faria <davidfaria89@gmail.com>
2021-01-26 15:36:02 -0300

    feat: install and make query games

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
@@ -13,10 +14,12 @@ export default function Index(props: HomeTemplateProps) {
 // getStaticProps => gerar estático em build time
 // getServerSideProps => gerar via ssr a cada request
 // getInitialProps => gerar via ssr a cada request
-export function getServerSideProps() {
-  // faz lógica
-  // pode ser buscar dados numa API
-  // fazer calculo|leitura de context
+export async function getServerSideProps() {
+  // const apolloClient = initializeApollo()
+
+  // const { data } = await apolloClient.query({
+  //   query: GET_GAMES
+  // })
 
   // retorno dos dados
   return {
