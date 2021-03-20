9c254c3856ac01b5bd8aa2160442daf7cccdfd1a
David Faria <davidfaria89@gmail.com>
2021-03-20 10:54:08 -0300

    feat: add protect route in the private pages

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -2,6 +2,8 @@ import OrdersList, { OrdersListProps } from 'components/OrdersList'
 import Profile from 'templates/Profile'
 
 import ordersMock from 'components/OrdersList/mock'
+import { GetServerSidePropsContext } from 'next'
+import protectedRoutes from 'utils/protected-routes'
 
 export default function Orders({ items }: OrdersListProps) {
   return (
@@ -11,9 +13,12 @@ export default function Orders({ items }: OrdersListProps) {
   )
 }
 
-export function getServerSideProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+
   return {
     props: {
+      session,
       items: ordersMock
     }
   }
--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -2,14 +2,19 @@ import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
 
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
+import protectedRoutes from 'utils/protected-routes'
+import { GetServerSidePropsContext } from 'next'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
   return <Wishlist {...props} />
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+
   return {
     props: {
+      session,
       games: gamesMock,
       recommendedGames: gamesMock.slice(0, 5),
       recommendedHighlight: highlightMock
